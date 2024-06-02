a15c84ca77a1d749ed2197cd15c43f4a4cef1907
Memfis <Memfis-745@yandex.ru>
2024-06-03 02:11:45 +0300

    T-6-2

--- Test/InMemoryTaskManagerTest.java
+++ Test/InMemoryTaskManagerTest.java
@@ -39,18 +39,7 @@ class InMemoryTaskManagerTest {
 
     }
 
-    @Test
-    void taskWithSameIdEquals() { // проверка на равенство двух экземпляров задач с одинаковым id
-        Task task1 = new Task("Test addNewTask", "Test addNewTask description");
-        task1.setHandId(1);
-        taskManager.addTask(task1);
 
-        final Task savedTask = taskManager.showTask(1);
-        final Task savedTaskCopy = taskManager.showTask(1);
-
-        // Проверка на равенство двух задач с одинаковыми полями, но разными id
-        assertEquals(savedTask, savedTaskCopy, "Задачи не совпадают.");
-    }
 
     @Test
     void equalsEpicWithSameId() { // проверка на равенство двух экземпляров наследников Task с одинаковым id
